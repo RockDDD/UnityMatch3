a5f09eef8208165da1a252680ac4b468a7980722
caifenglei <465166328@qq.com>
2017-07-05 21:53:05 +0800

    add viewport resize event delegate

--- Source/MyUnrealMatch/Match3GameInstance.cpp
+++ Source/MyUnrealMatch/Match3GameInstance.cpp
@@ -6,3 +6,24 @@
 
 
 
+UMatch3GameInstance::UMatch3GameInstance()
+{
+
+}
+
+void UMatch3GameInstance::Init()
+{
+	ViewportHandle = FViewport::ViewportResizedEvent.AddUObject(this, &UMatch3GameInstance::OnViewportResize_Internal);
+	Super::Init();
+}
+
+void UMatch3GameInstance::Shutdown()
+{
+	FViewport::ViewportResizedEvent.Remove(ViewportHandle);
+	Super::Shutdown();
+}
+
+void UMatch3GameInstance::OnViewportResize_Internal(FViewport* Viewport, uint32 ID)
+{
+	OnViewportResize();
+}
--- Source/MyUnrealMatch/Match3GameInstance.h
+++ Source/MyUnrealMatch/Match3GameInstance.h
@@ -12,8 +12,17 @@ UCLASS()
 class MYUNREALMATCH_API UMatch3GameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+public:
+	UMatch3GameInstance();
+	void Init() override;
+	void Shutdown() override;
+
+	// Internal function to bind to viewport resizing delegate
+	void OnViewportResize_Internal(FViewport* Viewport, uint32 ID);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = UI)
+	void OnViewportResize();
+
+private:
+	FDelegateHandle ViewportHandle;
 };
