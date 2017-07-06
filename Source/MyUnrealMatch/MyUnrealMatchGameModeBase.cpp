cd1a94d746f0a596a17711932905d4a2176538cc
caifenglei <465166328@qq.com>
2017-07-06 21:17:45 +0800

    gamemodeBase

--- Source/MyUnrealMatch/MyUnrealMatchGameModeBase.cpp
+++ Source/MyUnrealMatch/MyUnrealMatchGameModeBase.cpp
@@ -4,6 +4,7 @@
 #include "MyUnrealMatchGameModeBase.h"
 #include "GameFramework/Pawn.h"
 #include "Match3PlayerController.h"
+#include "Match3BlueprintFunctionLibrary.h"
 
 AMyUnrealMatchGameModeBase::AMyUnrealMatchGameModeBase(const FObjectInitializer& ObjectInitializer)
 	:Super(ObjectInitializer)
@@ -45,6 +46,158 @@ void AMyUnrealMatchGameModeBase::GameOver()
 	if (bGameWillBeWon)
 	{
 		UMatch3GameInstance* GameInstance = Cast<UMatch3GameInstance>(UGameplayStatics::GetGameInstance(this));
-		//if(AMatch3PlayerController* PC = Cast<AMatch3PlayerController>())
+		if (AMatch3PlayerController* PC = Cast<AMatch3PlayerController>(UMatch3BlueprintFunctionLibrary::GetLocalPlayerController(this)))
+		{
+			SaveGameData.TopScore = FMath::Max(PC->GetScore(), SaveGameData.TopScore);
+		}
+		GameInstance->UpdateSave(this, SaveGameData);
+		GameInstance->SaveGame();
+	}
+	ChangeMenuWidget(bGameWillBeWon ? VictoryWidgetClass : DefeatWidgetClass);
+	GameWasWon(bGameWillBeWon);
+}
+
+bool AMyUnrealMatchGameModeBase::IsGameActive() const
+{
+	FTimerManager& WorldTimerManager = GetWorldTimerManager();
+	return (WorldTimerManager.IsTimerActive(GameOverTimer) || WorldTimerManager.IsTimerPaused(GameOverTimer));
+}
+
+void AMyUnrealMatchGameModeBase::PauseGameTimer(bool bPause)
+{
+	if (bPause)
+	{
+		GetWorldTimerManager().PauseTimer(GameOverTimer);
+	}
+	else
+	{
+		GetWorldTimerManager().UnPauseTimer(GameOverTimer);
+	}
+}
+
+FString AMyUnrealMatchGameModeBase::GetRemainingTimeAsString()
+{
+	int32 OutInt = FMath::CeilToInt(GetWorldTimerManager().GetTimerRemaining(GameOverTimer));
+	return FString::Printf(TEXT("%03i"), FMath::Max(0, OutInt));
+}
+
+bool AMyUnrealMatchGameModeBase::GetTimerPaused()
+{
+	return GetWorldTimerManager().IsTimerPaused(GameOverTimer);
+}
+
+void AMyUnrealMatchGameModeBase::AddScore(int32 Points)
+{
+	if (AMatch3PlayerController* PC = Cast<AMatch3PlayerController>(UMatch3BlueprintFunctionLibrary::GetLocalPlayerController(this)))
+	{
+		int32 OldScore = PC->GetScore();
+		PC->AddScore(Points);
+		int32 NewScore = PC->GetScore();
+		if (NewScore >= SaveGameData.BronzeScore)
+		{
+			bGameWillBeWon = true;
+		}
+
+		if (NewScore > SaveGameData.GoldScore)
+		{
+			FinalPlace = 1;
+			AwardPlace(1, Points);
+		}
+		else if (NewScore > SaveGameData.SilverScore)
+		{
+			FinalPlace = 2;
+			AwardPlace(2, Points);
+		}
+		else if (NewScore > SaveGameData.BronzeScore)
+		{
+			FinalPlace = 3;
+			AwardPlace(3, Points);
+		}
+		else
+		{
+			FinalPlace = 0;
+			AwardPlace(0, Points);
+		}
+
+		for (const FMatch3Reward& Reward : Rewards)
+		{
+			check(Reward.ScoreInterval > 0);
+			int32 ScoreAwardCount = (NewScore / Reward.ScoreInterval) - (OldScore / Reward.ScoreInterval);
+			if (ScoreAwardCount > 0)
+			{
+				float StartingTimeValue = GetWorldTimerManager().GetTimerRemaining(GameOverTimer);
+				if (StartingTimeValue >= 0.0f)
+				{
+					GetWorldTimerManager().SetTimer(GameOverTimer, this, &AMyUnrealMatchGameModeBase::GameOver, StartingTimeValue + (ScoreAwardCount * Reward.TimeAwarded),false);
+					AwardBonus();
+				}
+			}
+		}
+
+	}
+}
+
+void AMyUnrealMatchGameModeBase::UpdateScoresFromLeaderBoard(int32 GoldScore, int32 SilverScore, int32 BronzeScore)
+{
+	UMatch3GameInstance* GameInstance = Cast<UMatch3GameInstance>(UGameplayStatics::GetGameInstance(this));
+	SaveGameData.BronzeScore = BronzeScore;
+	SaveGameData.SilverScore = SilverScore;
+	SaveGameData.GoldScore = GoldScore;
+	GameInstance->SaveGame();
+}
+
+void AMyUnrealMatchGameModeBase::SetComboPower(int32 NewComboPower)
+{
+	if (AMatch3PlayerController* PC = Cast<AMatch3PlayerController>(UMatch3BlueprintFunctionLibrary::GetLocalPlayerController(this)))
+	{
+		PC->ComboPower = NewComboPower;
+	}
+}
+
+int32 AMyUnrealMatchGameModeBase::GetComboPower()
+{
+	if (AMatch3PlayerController* PC = Cast<AMatch3PlayerController>(UMatch3BlueprintFunctionLibrary::GetLocalPlayerController(this)))
+	{
+		return PC->ComboPower;
+	}
+	return 0;
+}
+
+int32 AMyUnrealMatchGameModeBase::GetMaxComboPower()
+{
+	if (AMatch3PlayerController* PC = Cast<AMatch3PlayerController>(UMatch3BlueprintFunctionLibrary::GetLocalPlayerController(this)))
+	{
+		return PC->MaxComboPower;
+	}
+	return 0;
+}
+
+int32 AMyUnrealMatchGameModeBase::CalculateBombPower_Implementation()
+{
+	if (AMatch3PlayerController* PC = Cast<AMatch3PlayerController>(UMatch3BlueprintFunctionLibrary::GetLocalPlayerController(this)))
+	{
+		return PC->CalculateBombPower();
+	}
+	return 0;
+}
+
+int32 AMyUnrealMatchGameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	if (CurrentWidget)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+
+	if (NewWidgetClass)
+	{
+		if (AMatch3PlayerController* PC = Cast<AMatch3PlayerController>(UMatch3BlueprintFunctionLibrary::GetLocalPlayerController(this)))
+		{
+			CurrentWidget = CreateWidget<UUserWidget>(PC, NewWidgetClass);
+			if (CurrentWidget)
+			{
+				CurrentWidget->AddToViewport();
+			}
+		}
 	}
 }
